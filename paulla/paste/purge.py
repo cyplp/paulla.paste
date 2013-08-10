0cb4f52fa85dbdc67d8a28a5327f3ab45965e26e
Cyprien Le Pannérer <cyplp@free.fr>
2013-08-10 20:42:44 +0200

    purge a real app

--- paulla/paste/purge.py
+++ paulla/paste/purge.py
@@ -1,3 +1,8 @@
+import argparse
+import ConfigParser
+import logging
+import logging.config
+
 import couchdbkit
 
 from paulla.paste.models import Paste
@@ -11,12 +16,24 @@ def main():
     """
     Purge old paste.
     """
-    server = couchdbkit.Server(settings['couchdb.url'])
-    db = server.get_or_create_db(settings['couchdb.db'])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--conf',
+                        help='paulla.paste conf file')
+
+    args = parser.parse_args()
+
+    config = ConfigParser.RawConfigParser()
+    config.read(args.conf)
+
+    logging.config.fileConfig(args.conf)
+    logger = logging.getLogger('purge')
+
+    server = couchdbkit.Server(config.get('app:main', 'couchdb.url'))
+    db = server.get_or_create_db(config.get('app:main','couchdb.db'))
     Paste.set_db(db)
 
     oldPastes = Paste.view('old/all').all()
 
     for paste in oldPastes:
-        print paste._id
+        logger.info("deleting %s", paste._id)
         paste.delete()
